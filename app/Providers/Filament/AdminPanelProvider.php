89b422cf62c28c0b7fa795d898e6a1741f09b685
Dilovar <dilovar09@gmail.com>
2025-10-31 18:36:41 +0300

    some changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,7 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 //Dashboard::class,
             ])
+            ->globalSearch(false)
             ->sidebarWidth('13rem')
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
